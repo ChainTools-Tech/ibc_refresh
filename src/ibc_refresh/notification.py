77808f8da375fe50209872a2014ba05987439afb
qf3l3k <qf3l3k@gmail.com>
2025-02-14 10:26:42 +0100

    Notification datetime formatting updated for backwards comaptibility.

--- src/ibc_refresh/notification.py
+++ src/ibc_refresh/notification.py
@@ -16,7 +16,7 @@ class NotificationHandler:
             "info": 3066993        # Blue
         }
         embed_color = color_map.get(severity, 3066993)
-        timestamp = datetime.now(datetime.UTC)
+        timestamp = datetime.utcnow().isoformat() + "Z"
 
         for destination in self.notifications:
             notify_type = destination.get("type")
